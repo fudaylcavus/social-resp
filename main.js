49d7d53a95d7ca6b553c4f55e341ba632c5617a7
fudaylcavus <fudaylcavus@gmail.com>
2022-01-07 15:03:41 +0300

    first presentable

---
+++ main.js
@@ -0,0 +1,9 @@
+// window.onload = () => {
+//     console.log('hello world')
+//     setTimeout(() => {
+//         window.scrollTo(0, window.screen.height);
+//     }, 3000)
+// }
+const handleEmailClick = (e) => {
+    console.log(e)
+}
